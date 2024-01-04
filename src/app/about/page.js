70c40f9b3b5249a7143ab1899bf1921b9ceabf01
Taryn Martin <taryn.martin919@gmail.com>
2024-01-03 17:09:07 -0700

    feature: added pills to display skills

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,18 @@
 import styles from './page.module.css'
 import { Raleway } from 'next/font/google';
+// icons
+import { FaReact } from "react-icons/fa6";
+import { FaVuejs } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaHtml5 } from "react-icons/fa6";
+import { IoLogoCss3 } from "react-icons/io";
+import { FaSass } from "react-icons/fa6";
+import { FiFigma } from "react-icons/fi";
+import { TbBrandCypress } from "react-icons/tb";
+import { SiJest } from "react-icons/si";
+import { BsBootstrap } from "react-icons/bs";
+// components
+import Pill from '../components/Pill/Pill';
 
 const raleway = Raleway({ subsets: ['latin-ext'] });
 
@@ -12,6 +25,19 @@ const About = () => {
     focus: 'Front End Development',
     email: 'taryn@duck.com'
   }
+
+  const programmingInfo = [
+    { text: 'React', image: <FaReact size={32} /> },
+    { text: 'Vue', image: <FaVuejs size={32} /> },
+    { text: 'Javascript', image: <IoLogoJavascript size={32} /> },
+    { text: 'HTML', image: <FaHtml5 size={32} /> },
+    { text: 'CSS', image: <IoLogoCss3 size={32} /> },
+    { text: 'SASS', image: <FaSass size={32} />},
+    { text: 'Figma', image: <FiFigma size={32} /> },
+    { text: 'Cypress', image: <TbBrandCypress size={32} /> },
+    { text: 'Jest', image: <SiJest size={32} />},
+    { text: 'Bootstrap', image: <BsBootstrap size={32} /> },
+  ]
 
   const AboutInfoSection = ({info}) => {
     return (
@@ -40,8 +66,13 @@ const About = () => {
       </div>
       <h1 className={styles.header}>Programming</h1>
       <div className={raleway.className}>
-        <section className={styles.section}>
-          {/* create pills with icons for react, vue, javascript, html, css, figma, cypress, bootstrap? */}
+        <section className={styles.programmingSection}>
+          {programmingInfo.map((info) => {
+            return (
+              <Pill text={info.text} image={info.image} />
+            )
+          })
+          }
         </section>
       </div>
       <h1 className={styles.header}>Experience</h1>
