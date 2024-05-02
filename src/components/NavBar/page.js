6d40456ab2c56973bdee661c4991a79aad81c412
Taryn Martin <taryn.martin919@gmail.com>
2024-05-02 10:58:28 -0600

    fix: fixed issue with active tab

--- src/components/NavBar/page.js
+++ src/components/NavBar/page.js
@@ -1,27 +1,16 @@
 'use client'
 
-import { useEffect, useState } from "react"
 import { usePathname } from "next/navigation";
 import Link from "next/link"
 import styles from "./page.module.css"
 
 export default function NavBar() {
   const pathname = usePathname();
-  const [active, setActive] = useState('/home');
-
-  useEffect(() => {
-    console.log('check', pathname, active)
-    if (pathname === '/' && active === '/home') {
-      return
-    } else if (pathname !== active) {
-      setActive(pathname);
-    }
-  }, [])
 
   return (
-    <div className={styles.navBar}>
-      <Link href="/" onClick={() => setActive('/home')} className={active === '/home' ? styles.activeLink : styles.link}>Home</Link>
-      <Link href="/about" onClick={() => setActive('/about')} className={active === '/about' ? styles.activeLink : styles.link}>About</Link>
-    </div>
+    <nav className={styles.navBar}>
+      <Link href="/" className={pathname === '/' ? styles.activeLink : styles.link}>Home</Link>
+      <Link href="/about" className={pathname === '/about' ? styles.activeLink : styles.link}>About</Link>
+    </nav>
   )
 }
