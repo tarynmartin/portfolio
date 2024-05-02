2f2c5869253d78fb796e79240573113cdaf3e691
Taryn Martin <taryn.martin919@gmail.com>
2024-05-02 13:49:45 -0600

    feature: played with text animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hi!</h1>
+    <div className={styles.home}>
+      <h1>Taryn Martin</h1>
+      <span>Brings </span>
+      <ul className={styles.dynamic}>
+        <li>dedication</li>
+        <li>creativity</li>
+        <li>experience</li>
+        <li>blah</li>
+      </ul>
     </div>
   )
 }
