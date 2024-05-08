7ad5209d8ccb714914771bd1fce931242f346c40
Taryn Martin <taryn.martin919@gmail.com>
2024-05-08 11:36:49 -0600

    feature: finished text fade and changed background color for content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import TextFade from '../components/TextFade/TextFade'
 import styles from './page.module.css'
 
 export default function Home() {
-  const descriptors = ['Dedication', 'Creativity', 'Experience', 'Quality'];
-
   return (
     <div className={styles.home}>
       <h1>Taryn Martin</h1>
-      <TextFade staticText='Brings' dynamicText={descriptors} />
+      <TextFade staticText='Brings' />
     </div>
   )
 }
--- src/components/TextFade/TextFade.js
+++ src/components/TextFade/TextFade.js
@@ -1,32 +1,11 @@
-'use client'
-
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './TextFade.module.css'
 
-const TextFade = ({ staticText, dynamicText }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    // on refresh, transition becomes out of sync
-    console.log('currentIndex:', currentIndex);
-    let timerID = setTimeout(() => changeCurrentIndex(), 4000);
-    
-    return () => {
-      clearTimeout(timerID);
-    }
-
-  }, [currentIndex]);
-
-  const changeCurrentIndex = () => {
-    setCurrentIndex(currentIndex < dynamicText.length - 1 ? currentIndex + 1 : 0);
-  }
-
+const TextFade = ({ staticText }) => {
   return (
     <div className={styles.textFadeContainer}>
         <p>{staticText}</p>
-        <ul className={styles.textFadeList}>
-          <li className={styles.textFadeItem}>{dynamicText[currentIndex]}</li>
-        </ul>
+        <p className={styles.textFadeItem}></p>
       </div>
   )
 }
